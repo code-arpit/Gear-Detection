3ea1524f1d465e7f2cf24fbad7d7e78efb4082d9
Arpit Jain <83284168+code-arpit@users.noreply.github.com>
2021-06-21 08:25:38 +0000

    Update Config.py

--- Config.py
+++ Config.py
@@ -34,6 +34,10 @@ def getCanny_thresh1():
 def getCanny_thresh2():
     return configDict["Canny_thresh2"]
 
+def getEquat_teeth():
+    return configDict["Equat_teeth"]    
+
+
 def getMin_Near_Curve():
     return configDict["Min_Near_Curve"]
 
